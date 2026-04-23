60776c6dbcc6422ca453e47c6138ff6e27b1bea7
Nacho <ignaciopanelo114@gmail.com>
2026-04-23 00:39:56 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+// Express usa EJS para renderizar las vistas
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+// Archivos estaticos (CSS, imagenes)
+app.use(express.static('assets'));
+
+// -- RUTAS --
+
+app.get('/', (req, res) => {
+    res.render('pages/index');
+});
+
+app.get('/products', (req, res) => {
+    res.render('pages/product');
+})
+
+app.get('/cart', (req, res) => {
+    res.render('pages/cart');
+})
+
+app.get('/checkout', (req, res) => {
+    res.render('pages/checkout');
+})
+
+app.get('/register', (req, res) => {
+    res.render('pages/register');
+})
+
+app.get('/login', (req, res) => {
+    res.render('pages/login');
+})
+
+// -- ARRANCAR EL SERVIDOR --
+
+app.listen(PORT, () => {
+    console.log('Servidor funcionando en http://localhost:${PORT}');
+})
