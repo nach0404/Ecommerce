a2857452fee1739a1406d1ac54666f764d256473
Gab <proyectxs2000@gmail.com>
2026-05-04 20:49:34 -0300

    añade product.js. modificaciones en index, productCard, app

---
+++ Data/product.js
@@ -0,0 +1,178 @@
+const products = [
+  // ALIMENTOS
+  { id: 1, name: "Galletas Integrales", 
+    category: "Alimentos",
+    image: "",
+    points: "",
+    description: ""
+   },
+
+  { id: 2, name: "Quaker", 
+    category: "Alimentos",
+    image: "",
+    points: "",
+    description: ""
+},
+
+  { id: 3, name: "Paquete de Rapiditas", 
+    category: "Alimentos",
+    image: "",
+    points: "",
+    description: ""
+},
+
+  // BEBIDAS
+  { id: 4, name: "Whisky Escocés", 
+    category: "Bebidas",
+    image: "",
+    points: "",
+    description: "" 
+},
+
+  { id: 5, name: "Jugo Baggio", 
+    category: "Bebidas",
+    image: "",
+    points: "",
+    description: ""
+},
+
+  { id: 6, name: "Cerveza Heineken", 
+    category: "Bebidas",
+    image: "",
+    points: "",
+    description: ""
+
+},
+
+  // ELECTRONICA
+  { id: 7, name: "Auriculares Bluetooth", 
+    category: "Electronica",
+    image: "",
+    points: "",
+    description: ""
+},
+
+  { id: 8, name: "Smartwatch", 
+    category: "Electronica",
+    image: "",
+    points: "",
+    description: ""
+},
+
+  { id: 9, name: "Celular Iphone", 
+    category: "Electronica",
+    image: "",
+    points: "",
+    description: ""
+},
+
+  // INDUMENTARIA
+  { id: 10, name: "Pantalones", 
+    category: "Indumentaria",
+    image: "",
+    points: "",
+    description: ""
+ },
+
+  { id: 11, name: "Suéter", 
+    category: "Indumentaria",
+    image: "",
+    points: "",
+    description: ""
+ },
+
+  { id: 12, name: "Blusas", 
+    category: "Indumentaria",
+    image: "",
+    points: "",
+    description: ""
+ },
+
+  // JUEGOS
+  { id: 13, name: "Juegos de Cartas - Uno", 
+    category: "Juegos",
+    image: "",
+    points: "",
+    description: ""
+ },
+
+  { id: 14, name: "Juego de Mesa - Domino", 
+    category: "Juegos",
+    image: "",
+    points: "",
+    description: ""
+ },
+
+  { id: 15, name: "Nintendo Switch", 
+    category: "Juegos",
+    image: "",
+    points: "",
+    description: ""
+ },
+
+  // AUTOMOTOR
+  { id: 16, name: "Aceite de Motor", 
+    category: "Automotor",
+    image: "",
+    points: "",
+    description: ""
+ },
+
+  { id: 17, name: "Kit de Herramientas", 
+    category: "Automotor",
+    image: "",
+    points: "",
+    description: ""
+ },
+
+  { id: 18, name: "Cargador de Batería", 
+    category: "Automotor",
+    image: "",
+    points: "",
+    description: ""
+ },
+
+  // HOGAR
+  { id: 19, name: "Sofá 3 Cuerpos", 
+    category: "Hogar",
+    image: "",
+    points: "",
+    description: ""
+ },
+
+  { id: 20, name: "Mesa de Auxiliar", 
+    category: "Hogar",
+    image: "",
+    points: "",
+    description: ""
+ },
+
+  { id: 21, name: "Silla", 
+    category: "Hogar",
+    image: "",
+    points: "",
+    description: ""
+ },
+
+  // OTROS
+  { id: 22, name: "Tarjeta de Regalo", 
+    category: "Otros",
+    image: "",
+    points: "",
+    description: ""
+ },
+  { id: 23, name: "Mochila Urbana", 
+    category: "Otros",
+    image: "",
+    points: "",
+    description: ""
+ },
+  { id: 24, name: "Set de Regalo", 
+    category: "Otros",
+    image: "",
+    points: "",
+    description: ""
+ }
+];
+
+module.exports = products;
--- app.js
+++ app.js
@@ -2,43 +2,58 @@ const express = require('express');
 const app = express();
 const PORT = 5500;
 
-// Express usa EJS para renderizar las vistas
+const products = require("./data/products");
+
+// generar categorías dinámicas
+const categorias = [...new Set(products.map(p => p.category))];
+
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
-// Archivos estaticos (CSS, imagenes)
+//Archivos estaticos
 app.use(express.static('assets'));
 
 app.use(express.urlencoded({ extended: true }));
 
 // -- RUTAS --
 
+// HOME
 app.get('/', (req, res) => {
-    res.render('pages/index');
+  res.render('pages/index', {
+    products,
+    categorias
+  });
 });
 
-app.get('/products', (req, res) => {
-    res.render('pages/product');
-})
+// DETALLE DE PRODUCTO
+app.get('/products/:id', (req, res) => {
+  const product = products.find(p => p.id == req.params.id);
 
+  if (!product) {
+    return res.status(404).send("Producto no encontrado");
+  }
+
+  res.render('pages/product', { product });
+});
+
+// OTRAS PÁGINAS
 app.get('/cart', (req, res) => {
-    res.render('pages/cart');
-})
+  res.render('pages/cart');
+});
 
 app.get('/checkout', (req, res) => {
-    res.render('pages/checkout');
-})
+  res.render('pages/checkout');
+});
 
 app.get('/register', (req, res) => {
-    res.render('pages/register');
-})
+  res.render('pages/register');
+});
 
 app.get('/login', (req, res) => {
-    res.render('pages/login');
-})
+  res.render('pages/login');
+});
 
-// -- ARRANCAR EL SERVIDOR --
-
+// SERVER
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
